8a2adc35cab5fcc1a12e458fc0fceae1a9c41378
Martian <martdudn@icloud.com>
2026-05-04 10:13:40 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,7 +10,6 @@ export function getImagesByQuery(query) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 40,
       },
     })
     .then(res => {
